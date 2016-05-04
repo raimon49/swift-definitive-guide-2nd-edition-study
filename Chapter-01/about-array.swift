56a8c156f87ea81abf0f01686ab5061ed121ab6a
raimon <raimon49@hotmail.com>
2016-05-04 15:18:40 +0900

    Add code as Chapter-01

---
+++ Chapter-01/about-array.swift
@@ -0,0 +1,11 @@
+let digits = [ "00", "01", "02", "03", ]
+print(digits[2])
+print(digits.count) // ケツカンマを入れても要素数は4
+
+var roman = ["I", "II", "III"]
+roman.append("IV")
+print(roman)
+let m = roman + ["5", "6"]
+print(m)
+roman += ["V", "VI"]
+print(roman)
---
+++ Chapter-01/about-print-func.swift
@@ -0,0 +1,7 @@
+let n = 8
+let str = "\(n)の2乗は\(n+n)です。"
+print(str)
+
+print(6, 2014, "林檎")
+
+print(6, 2014, "林檎", separator:"; ")
